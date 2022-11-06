48bb6560312a75e4edb9ce39d24c6b002a3c7ecc
Oksanaka848 <okmitka@mail.ru>
2022-11-06 20:28:48 +0300

    ch

--- rand.js
+++ rand.js
@@ -12,33 +12,28 @@ button.onclick=ranDomf2;
     let sum  = document.getElementById('sum');
     let mult  = document.getElementById('mult');
     let arr = [];
-    
-    
-    function ranDomf2 () {
+        function ranDomf2 () {
         function ranDomf(min, max) {
-                    return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
                     } 
 for ( i=0; i<10; i++) {
 arr.push(ranDomf(-10, 10));}
-
 console.log(arr);
-
 numbs.value=arr;
-numbs.innerHTML+=numbs.value;
+numbs.innerHTML=numbs.value;
 let summ = arr.reduce((sum, i) => sum + i);
 sum.value=summ;
-sum.innerHTML+=sum.value;
+sum.innerHTML=sum.value;
 let multt = arr.reduce((mult, i) => mult * i);   
 mult.value=multt;
-mult.innerHTML+=mult.value;
+mult.innerHTML=mult.value;
 let minnumb2=Math.min.apply(null,arr);
 minnumb.value=minnumb2;
-minnumb.innerHTML+=minnumb.value; 
+minnumb.innerHTML=minnumb.value; 
 let maxnumb2=Math.max.apply(null,arr);
 maxnumb.value=maxnumb2;
-maxnumb.innerHTML+=maxnumb.value;   
+maxnumb.innerHTML=maxnumb.value;   
 let midnumb2 = sum.value / arr.length;
 midnumb.value=midnumb2;
-midnumb.innerHTML+=midnumb.value;
-
+midnumb.innerHTML=midnumb.value;
 } 
